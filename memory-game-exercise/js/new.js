061595ce7a521159e9f8d2bbaa0384ea9112ac7a
JuKoNi <92727651+JuKoNi@users.noreply.github.com>
2022-01-27 21:42:29 +0100

    ta-da

--- memory-game-exercise/js/new.js
+++ memory-game-exercise/js/new.js
@@ -4,7 +4,7 @@
  * välj ett kort, flippa, spara värdet i flippedCards
  * välj ett till kort, flippa, spara värdet i flippedCards
  * jämför de två värderna i flippedCards
- * OM lika, pusha till hittadePar och (ta bort från skärmen), ta bort sparade värden flippedCards
+ * OM lika, pusha till hittadePar, ta bort sparade värden flippedCards
  * OM fel, vänd tillbaka, ta bort sparade värden från flippedCards
  * när hittadePar = 8, DU VANN
  * knapp: Spela igen? - reset 
@@ -14,20 +14,30 @@
  const allCards = document.querySelector('.memory-cards')
  const cardElem = document.querySelectorAll('.memory-card');
  const backCardElem = document.querySelectorAll('.back');
- const frontCardElem = document.querySelectorAll('.front')
  const winElem = document.querySelector('.overlay');
  const closeElem = document.querySelector('.close')
+ const buttonElem = document.querySelectorAll('.btn');
 
  let flippedCards = []
  let foundPairs = []
 
+ // Shufflar korten
+ for (let i = allCards.children.length; i >= 0; i--) {
+    allCards.appendChild(allCards.children[Math.random() * i | 0]);
+}
+
+// Reloadar sidan när en klickar på Starta om/Spela igen-knappen
+ for(let i = 0; i < buttonElem.length; i++) {
+     buttonElem[i].addEventListener(`click`, () => {
+         window.location.reload(true);
+     })
+ }
+
+ // Stänger ner overlayen på X när en vunnit
  closeElem.addEventListener(`click`, () => {
     winElem.classList.remove('show')
 })
 
-// for (let i = allCards.children.length; i >= 0; i--) {
-//     allCards.appendChild(allCards.children[Math.random() * i | 0]);
-// }
 
 
  function flipCard(flip) {
@@ -56,24 +66,29 @@ function compareCards() {
     }
 }
 
+
+
 function howManyPairs() {
     if(foundPairs.length / 2 == 8) {
-        winElem.classList.add('show');
+        setTimeout(function() { winElem.classList.add('show');}, 1500);
     }
 }
 
 
- for(let i = 0; i < backCardElem.length; i++) {
+ for(let i = 0; i < cardElem.length; i++) {
      backCardElem[i].addEventListener(`click`, () => {
-         
+         // kontroll att man inte har flippat fler än 2 kort
+         if(flippedCards.length == 4) {
+             return;
+         }
          flipCard([cardElem[i]]);
 
          flippedCards.push(cardElem[i].dataset.card , cardElem[i]);
          console.log(flippedCards);
-         
 
          compareCards();
          howManyPairs();
+
      })
  }
 
